24880d2868d7c329a69c91e60a2783a673b322cc
DELAHAIE Marion <marion.delahaie@cstb.fr>
2019-12-02 16:04:56 +0100

    average lib to built

---
+++ lib/average.py
@@ -0,0 +1,10 @@
+import statistics
+
+class average:
+    def __init__(self, Datas):
+        self.Datas = Datas
+    
+    def average(self, Datas):
+        averaged_datas = []
+        for row in 
+        return averaged_datas
